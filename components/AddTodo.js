0219b727dd285923c9eae4a9a601d0374268eed8
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2022-03-14 23:08:25 +0330

    add responsive for all devices

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { HStack, Input, Button, useToast } from '@chakra-ui/react'
+import { Input, Button, useToast, Box } from '@chakra-ui/react'
 import { nanoid } from 'nanoid'
 
 const AddTodo = ({ addTodo }) => {
@@ -29,19 +29,76 @@ const AddTodo = ({ addTodo }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <HStack mt='8'>
+    <form
+      onSubmit={handleSubmit}
+      style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+    >
+      <Box
+        mt='8'
+        d={{
+          sm: 'grid',
+          md: 'grid',
+          lg: 'flex',
+          xl: 'flex',
+          base: 'grid',
+        }}
+        justifyContent={{
+          sm: '',
+          md: '',
+          lg: 'center',
+          xl: 'center',
+          base: '',
+        }}
+        width={{
+          sm: '80vw',
+          md: '80vw',
+          lg: 'auto',
+          xl: 'auto',
+          base: '90vw',
+        }}
+      >
         <Input
           variant='filled'
           placeholder='learing programming...'
-          width='18rem'
+          width={{
+            sm: '100%',
+            md: '100%',
+            lg: '18rem',
+            xl: '18rem',
+            base: '100%',
+          }}
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
-        <Button colorScheme='pink' px='8' type='submit'>
+        <Button
+          colorScheme='pink'
+          px='8'
+          type='submit'
+          width={{
+            sm: '100%',
+            md: 'auto',
+            lg: 'auto',
+            xl: 'auto',
+            base: '100%',
+          }}
+          mt={{
+            sm: '2',
+            md: '2',
+            lg: '0',
+            xl: '0',
+            base: '2',
+          }}
+          ml={{
+            sm: '0',
+            md: '0',
+            lg: '2',
+            xl: '2',
+            base: '0',
+          }}
+        >
           Add Todo
         </Button>
-      </HStack>
+      </Box>
     </form>
   )
 }
